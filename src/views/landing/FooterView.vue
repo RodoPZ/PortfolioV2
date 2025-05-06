4c1695a2596f3b3f1546c754ab4803b1ba5cf0f2
RodoPZ <rpinedo2799@gmail.com>
2025-05-05 22:41:55 -0600

    Refactor FooterView layout and styling

--- src/views/landing/FooterView.vue
+++ src/views/landing/FooterView.vue
@@ -1,19 +1,15 @@
 <template>
 	<footer class="bg-slate-800" aria-labelledby="footer-heading">
 		<h2 id="footer-heading" class="sr-only">Footer</h2>
-		<div class="mx-auto max-w-md px-6 pt-12 sm:max-w-7xl lg:px-8 lg:pt-16">
+		<div class="mx-auto max-w-md px-6 py-12 sm:max-w-7xl lg:px-8 lg:py-16">
 			<div class="xl:gap-3 flex flex-col items-center">
-				<h1 class="text-4xl font-bold text-primary">
-					&lt;RodoPZ/&gt;
-				</h1>
-				<p class="text-base text-white">Designed and built by RodoPZ using Vue, TS and Tailwind</p>
-			</div>
-			<div class="mt-4 border-t border-gray-200 py-4">
-				<p class="text-base text-gray-400 xl:text-center">Designed and built by RodoPZ using Vue</p>
+				<h1 class="text-2xl font-bold text-primary lg:text-4xl">&lt;RodoPZ/&gt;</h1>
+				<p class="text-base text-white text-center">
+					Designed and built by RodoPZ using Vue, TS and Tailwind
+				</p>
 			</div>
 		</div>
 	</footer>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
